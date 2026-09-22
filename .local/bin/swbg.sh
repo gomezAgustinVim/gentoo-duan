1557e0589c4f14af7757708d7be82c87260fbdc5
Agustin <aguscea18@gmail.com>
2026-09-21 21:36:39 -0300

    wallpaper scripts for x and wayland

--- .local/bin/swbg.sh
+++ .local/bin/swbg.sh
@@ -3,10 +3,32 @@
 BGLOC="${XDG_DATA_HOME:-$HOME/.local/share}/bg"
 CURRENT_WALL="$(readlink -f "$BGLOC"/wall.*)"
 
-killall -9 swaybg
+mkdir -p "$BGLOC"
+pkill swaybg
 
-WALLPAPER_DIR="${XDG_PICTURES_DIR:-$HOME/Imágenes}/walls/nord/"
+if [ $# -eq 1 ]; then
+    trueloc="$(readlink -f "$1")"
+    MIME="$(file --mime-type -b "$trueloc")"
+    case "$MIME" in
+    image/*)
+        EXT="$(basename "${1##*.}")"
+        ln -sf "$trueloc" "$BGLOC"/wall."$EXT"
+        ;;
+    inode/directory)
+        WALLPAPER="$(find "$trueloc" -iregex '.*.\(jpg\|jpeg\|png\|gif\)' -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)"
+        EXT="$(basename "${WALLPAPER##*.}")"
+        ln -sf "$WALLPAPER" "$BGLOC"/wall."$EXT"
+        ;;
+    *)
+        notify-send "Mime incorrecto" "No es imagen ni directorio"
+        exit 1
+        ;;
+    esac
+    swaybg -m fill -i "$BGLOC"/wall."$EXT"
+fi
+
+WALLPAPER_DIR="${XDG_PICTURES_DIR:-$HOME/Imágenes}/walls/gentoo-walls"
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
 EXT="$(basename "${WALLPAPER##*.}")"
 ln -sf "$WALLPAPER" "$BGLOC"/wall."$EXT"
-swaybg -i "$WALLPAPER"
+swaybg -m fill -i "$WALLPAPER"
